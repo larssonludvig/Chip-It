45cb6221e37d40f8f99e96a8f295984df74b11b3
Canamo <ludviglarsson01@live.se>
2019-11-28 16:30:09 +0100

    Added Triggerboxes and some graphics

---
+++ Chip It/Assets/Scripts/WaterTriggerEvent.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class WaterTriggerEvent: MonoBehaviour
+{
+    private readonly PlayerInteraction instance;
+    private Vector2 resetVelocity;
+
+    /// <summary>
+    /// Checks for collision
+    /// </summary>
+    /// <param name="collision"></param>
+    private async void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.CompareTag("Player")) {
+            await Task.Delay(2000);
+            SetBackPlayer();
+        }
+    }
+
+    /// <summary>
+    /// Sets the player back to last location and adds 1 to the score
+    /// </summary>
+    public void SetBackPlayer() {
+        PlayerInteraction.instance.GetComponent<Rigidbody2D>().position = PlayerInteraction.instance.lastPosition;
+        PlayerInteraction.instance.GetComponent<Rigidbody2D>().velocity = resetVelocity;
+        PlayerInteraction.instance.IncreaseScore();
+    }
+}
--- Chip It/Assets/Scripts/playerInteraction.cs
+++ Chip It/Assets/Scripts/playerInteraction.cs
@@ -4,44 +4,82 @@ using UnityEngine;
 
 public class PlayerInteraction : MonoBehaviour
 {
+    public static PlayerInteraction instance;
+
+    public Vector2 lastPosition;
+
     // Variables for the mouse
     private Vector2 startPosition;
     private Vector2 endPosition;
     private Vector2 newVelocity;
-    private float speedMultiplier = 5;
+    private readonly int speedMultiplier = 6;
 
-    // Start is called before the first frame update
-    private void Start() {}
+    // Score
+    private int score = 0;
+    public bool finish = false;
 
-    // Update is called once per frame
-    private void Update() { }
+    /// <summary>
+    /// Starts an instance
+    /// </summary>
+    private void Start() {
+        instance = this;
+    }
 
-    // Checks if mouse button 1 is pressed down
+    /// <summary>
+    /// Upadtes the last location of the player
+    /// </summary>
+    private void Update() {
+        if (this.GetComponent<Rigidbody2D>().velocity.x == 0 && this.GetComponent<Rigidbody2D>().velocity.y == 0 && this.lastPosition != this.GetComponent<Rigidbody2D>().position) {
+            this.lastPosition = this.GetComponent<Rigidbody2D>().position;
+        }
+    }
+
+
+    /// <summary>
+    /// Checks if mouse button 1 is pressed down
+    /// </summary>
     private void OnMouseDown() {
         this.startPosition = Input.mousePosition;
         this.startPosition = Camera.main.ScreenToWorldPoint(this.startPosition); // Converts mouse positioning from screen to world
     }
 
-    // Checks if mouse button 1 is released
+    /// <summary>
+    /// Checks if mouse button 1 is released
+    /// </summary>
     private void OnMouseUp() {
         this.endPosition = Input.mousePosition;
         this.endPosition = Camera.main.ScreenToWorldPoint(this.endPosition); // Converts mouse positioning from screen to world 
 
-        if (this.GetComponent<Rigidbody2D>().velocity.x == 0 && this.GetComponent<Rigidbody2D>().velocity.y == 0) {
+        if (this.GetComponent<Rigidbody2D>().velocity.x <= 0.1 && this.GetComponent<Rigidbody2D>().velocity.y <= 0.1 && finish != true) {
             this.newVelocity = VelocityCalculation(this.startPosition, this.endPosition);
-
             this.GetComponent<Rigidbody2D>().velocity = this.newVelocity;
+            IncreaseScore();
         }
     }
 
-    // Runs when mouse 1 is pressed
+    /// <summary>
+    /// Runs when mouse 1 is pressed
+    /// </summary>
     private void OnMouseDrag() {}
 
-    // Calculates the differences between start and end position for mouse and produce a new velocity
+    /// <summary>
+    /// Calculates the differences between start and end position for mouse and produce a new velocity
+    /// </summary>
+    /// <param name="start"></param>
+    /// <param name="end"></param>
+    /// <returns></returns>
     private Vector2 VelocityCalculation(Vector2 start , Vector2 end) {
         Vector2 velocity = start;
         velocity.x = -(end.x - start.x) * this.speedMultiplier;
         velocity.y = -(end.y - start.y) * this.speedMultiplier;
         return velocity;
+    }
+
+    /// <summary>
+    /// Increases the score by 1
+    /// </summary>
+    public void IncreaseScore() {
+        score++;
+        Debug.Log(score);
     }
 }
